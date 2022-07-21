9031bcb2f6e6e2040bec819a435ace96162aa28f
Abul Kalam Azad <me.azad99@gmail.com>
2022-07-21 10:51:28 +0600

    react-pagination component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Images from './components/Images';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [images, setImages] = useState([]);
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/photos')
+			.then((response) => response.json())
+			.then((data) => setImages(data));
+	}, []);
+	return (
+		<div>
+			<Images data={images} />
+		</div>
+	);
 }
 
 export default App;
